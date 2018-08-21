3875257972c6e4881816def352ca4529be067e49
lerinaldojr <42380168+lerinaldojr@users.noreply.github.com>
2018-08-21 02:36:39 -0300

    Update LimpezaBBC_Lerinaldo.py

--- 2018.1/Lerinaldo/MT9/LimpezaBBC_Lerinaldo.py
+++ 2018.1/Lerinaldo/MT9/LimpezaBBC_Lerinaldo.py
@@ -2,7 +2,7 @@ import csv
 from datetime import datetime
 import os
 import pandas as pd
-
+ 
 def meucsv(path):
     retorno = None
     if os.path.isfile(path):
@@ -10,43 +10,46 @@ def meucsv(path):
             retorno = []
             linhas = []
             with open(path, 'rb') as csvfile:
-                r = csv.reader(csvfile, delimiter=' ', quotechar='|') 
+                r = csv.reader(csvfile, delimiter=' ', quotechar='|')
                 linha_count = 0
                 for linha in r:
-                    linhas.append(linha)
-                    #print linhas
-                    if len(linha)==2:
-                        try:
-                            data = datetime.strptime(linha[0], '%d/%m/%Y')
-                            resto_dividido = linha[1].split(',')
-                            resto = []
-                            for i in range(len(resto_dividido)):
-                                da_vez = resto_dividido[i]
-                                if len(da_vez)>0:
-                                    if len(da_vez)>4:
-                                        resto.append(da_vez)
-                                    else:
-                                        try:
-                                            valor = int(da_vez)
-                                        except Exception as e:
-                                            #print e
-                                            pass
+                    if linha_count < 100:
+                        linhas.append(linha)
+                        #print linhas
+                        if len(linha)==2:
+                            try:
+                                data = datetime.strptime(linha[0], '%d/%m/%Y')
+                                resto_dividido = linha[1].split(',')
+                                resto = []
+                                for i in range(len(resto_dividido)):
+                                    da_vez = resto_dividido[i]
+                                    if len(da_vez)>0:
+                                        if len(da_vez)>4:
+                                            resto.append(da_vez)
                                         else:
-                                            resto.append(valor)
-                            data = {"data": data.date().strftime('%d/%m/%Y'), "resto": ','.join(str(a) for a in resto)}
-                            retorno.append(data)
-                            linha_count += 1
-                        except Exception as e:
-                            #print e
-                            pass
+                                            try:
+                                                valor = int(da_vez)
+                                            except Exception as e:
+                                                #print e
+                                                pass
+                                            else:
+                                                resto.append(valor)
+                                data = {"data": data.date().strftime('%d/%m/%Y'), "resto": ','.join(str(a) for a in resto)}
+                                retorno.append(data)
+                                linha_count += 1
+                            except Exception as e:
+                                #print e
+                                pass
+                    else:
+                        break
         except Exception as e:
             #print e
             pass
     return retorno
-
-
-
+ 
+ 
+ 
 path = 'BBC-Results.csv'
 a = meucsv(path)
 csv_file = pd.DataFrame(a)
-csv_file.to_csv('BBC-Results-Lerinaldo.csv', sep='\t', encoding='utf-8')
+csv_file.to_csv('teste2.csv', sep='\t', encoding='utf-8')
