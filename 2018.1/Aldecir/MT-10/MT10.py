c7dbb24dc5024e2732170306d4feaad0b1d7b934
aldeci123 <40532815+aldeci123@users.noreply.github.com>
2018-09-04 04:14:09 -0300

    Update MT10.py

--- 2018.1/Aldecir/MT-10/MT10.py
+++ 2018.1/Aldecir/MT-10/MT10.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 if __name__ == "__main__":
 
-    arquivo = open('/Users/aldec/Desktop/Faculdade - 1/Faculdade/Mineração de Dados/MT10/mulheres_palestrantes.htm',mode="r", encoding="utf-8").read()
+    arquivo = open('mulheres_palestrantes.htm',mode="r", encoding="utf-8").read()
     pagina = etree.HTML(arquivo)
     
     nomes = pagina.xpath('//article[@class="card"]/h3/text()')
@@ -16,13 +16,6 @@ if __name__ == "__main__":
     participantes = []
     
     for nome,foto,localizacao in zip(nomes,fotos,localizacoes):
-        participantes.append({nome, foto, localizacao})
+        participantes.append((nome, foto, localizacao))
 
-for i, a in enumerate(participantes):
-    print (participantes[i])
-'''
-cards = pagina.xpath('//article[@class="card"]/ul')
-for card in cards: print ([[tema.text for tema in card.getchildren()]])
-temas_de_interesse = pagina.xpath('//article[@class="card"]/ul/li/text()')
-print (temas_de_interesse)
-'''
+print (participantes)
